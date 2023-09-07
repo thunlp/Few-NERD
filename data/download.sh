fe45e008d62981e14bd2d41bceca21c0025f02da
Yulin Chen <55496186+cyl628@users.noreply.github.com>
2023-09-07 17:34:19 +0800

    Update data download links

--- data/download.sh
+++ data/download.sh
@@ -5,15 +5,15 @@ bash $data_dir/download.sh intra
 bash $data_dir/download.sh supervised
 bash $data_dir/download.sh episode-data
 elif [ $1 == 'supervised' ]; then
-wget -O $data_dir/supervised.zip https://cloud.tsinghua.edu.cn/f/09265750ae6340429827/?dl=1
+wget -O $data_dir/supervised.zip https://cloud.tsinghua.edu.cn/f/c1f71c011d6b461786bc/?dl=1
 unzip -o -d $data_dir/ $data_dir/supervised.zip
 rm -rf $data_dir/supervised.zip
 elif [ $1 == 'inter' ]; then
-wget -O $data_dir/inter.zip https://cloud.tsinghua.edu.cn/f/165693d5e68b43558f9b/?dl=1
+wget -O $data_dir/inter.zip https://cloud.tsinghua.edu.cn/f/3d84d34dc5d845a2bed2/?dl=1
 unzip -o -d $data_dir/ $data_dir/inter.zip
 rm -rf $data_dir/inter.zip
 elif [ $1 == 'intra' ]; then
-wget -O $data_dir/intra.zip https://cloud.tsinghua.edu.cn/f/a0d3efdebddd4412b07c/?dl=1
+wget -O $data_dir/intra.zip https://cloud.tsinghua.edu.cn/f/a176a4870f0a4f8ba0db/?dl=1
 unzip -o -d $data_dir/ $data_dir/intra.zip
 rm -rf $data_dir/intra.zip
 elif [ $1 == 'episode-data' ]; then
